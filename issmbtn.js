11a5269d3b9774498b93053c7171dfada4e90c09
ortilex <33230389+ortilex@users.noreply.github.com>
2017-11-15 22:23:07 +0100

    Create issmbtn.js

---
+++ issmbtn.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+    var Menu = {
+
+        body: $('.issmenu'),
+        button: $('.button'),
+        tools: $('.tools')
+
+    };
+
+    Menu.button.click(function () {
+        Menu.body.toggleClass('menu--closed');
+        Menu.body.toggleClass('menu--open');
+        Menu.tools.toggleClass('tools--visible');
+        Menu.tools.toggleClass('tools--hidden');
+    });
+
+
+});
